04f356dd6db16023d6dc28f04cbbc63aba02f4e4
Jayant Shelke <jayantshelke4829@gmail.com>
2024-10-09 20:01:57 +0530

    updated

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 const port = process.env.PORT;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow requests from your frontend
+  credentials: true,
+}));
 app.use(express.json());
 
 // Connect to MongoDB
