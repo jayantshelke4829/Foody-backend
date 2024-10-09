cbe61a2008d54b247791a743b3620c3067a4e486
Jayant Shelke <jayantshelke4829@gmail.com>
2024-10-09 19:56:13 +0530

    updated

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
+// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const cartRoutes = require('./routes/cartRoutes'); 
+const cartRoutes = require('./routes/cartRoutes'); // Import routes
 require('dotenv').config()
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // Middleware
-// Allow CORS from specific origin or all origins
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://foody-backend-kjpp.onrender.com'],  // Add the origin of your frontend app
-  credentials: true // If your app requires credentials like cookies, tokens
-}));
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
@@ -25,7 +22,6 @@ mongoose.connect(process.env.MONGO_URL, {
 
 // Use cart routes
 app.use('/api', cartRoutes);
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
